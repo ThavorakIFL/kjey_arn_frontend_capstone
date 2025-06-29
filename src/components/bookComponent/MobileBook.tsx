a69f5b9161548ad220a733f7f4bfedcd8edd1146
ThavorakIFL <suon.thavorak.1521@rupp.edu.kh>
2025-06-29 16:24:59 +0700

    Removed testpage content. Will update after build is completed/.

--- src/components/bookComponent/MobileBook.tsx
+++ src/components/bookComponent/MobileBook.tsx
@@ -1,32 +1,32 @@
-"use client";
+// "use client";
 
-import { Book as BookType } from "@/types/book";
-import Link from "next/link";
-import React from "react";
-import StatusIndicator from "../StatusIndicator";
+// import { Book as BookType } from "@/types/book";
+// import Link from "next/link";
+// import React from "react";
+// import StatusIndicator from "../StatusIndicator";
 
-type MobileBookProps = {
-    book: BookType;
-};
+// type MobileBookProps = {
+//     book: BookType;
+// };
 
-const MobileBook: React.FC<MobileBookProps> = ({ book }) => {
-    return (
-        <Link href={`/books/${book.id}`}>
-            <div className="w-40 aspect-[4/6] relative group rounded-lg overflow-hidden bg-gray-200">
-                <img
-                    className="text-center h-full w-full object-cover"
-                    src="https://th.bing.com/th/id/OIP.rb6aTeGCgJp_H4hAi-DMCwHaL0?rs=1&pid=ImgDetMain"
-                    alt={`${book.title} cover`}
-                />
-            </div>
-            <p>{book.title}</p>
-            <div>
-                {book.genres.map((genre) => (
-                    <span key={genre.id}>{genre.genre}</span>
-                ))}
-            </div>
-        </Link>
-    );
-};
+// const MobileBook: React.FC<MobileBookProps> = ({ book }) => {
+//     return (
+//         <Link href={`/books/${book.id}`}>
+//             <div className="w-40 aspect-[4/6] relative group rounded-lg overflow-hidden bg-gray-200">
+//                 <img
+//                     className="text-center h-full w-full object-cover"
+//                     src="https://th.bing.com/th/id/OIP.rb6aTeGCgJp_H4hAi-DMCwHaL0?rs=1&pid=ImgDetMain"
+//                     alt={`${book.title} cover`}
+//                 />
+//             </div>
+//             <p>{book.title}</p>
+//             <div>
+//                 {book.genres.map((genre) => (
+//                     <span key={genre.id}>{genre.genre}</span>
+//                 ))}
+//             </div>
+//         </Link>
+//     );
+// };
 
-export default MobileBook;
+// export default MobileBook;
