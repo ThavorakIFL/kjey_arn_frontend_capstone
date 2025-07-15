a8ada148e43bdab15a92489984cbcfbfa40931f1
ThavorakIFL <suon.thavorak.1521@rupp.edu.kh>
2025-07-15 08:38:15 +0700

    fixed not found issue

--- src/app/(protected)/user-profile/[id]/page.tsx
+++ src/app/(protected)/user-profile/[id]/page.tsx
@@ -2,6 +2,35 @@ import { notFound } from "next/navigation";
 import OtherUserProfilePage from "../OtherUserPageClient";
 import { getOtherUserProfile, getOtherUserBooks } from "../user-profile-action";
 
+// export default async function UserProfilePage({
+//     params,
+// }: {
+//     params: Promise<{ id: string }>;
+// }) {
+//     const { id } = await params;
+
+//     try {
+//         const otherUserProfile = await getOtherUserProfile(id);
+//         const otherUserBookDataRes = await getOtherUserBooks(id);
+//         const otherUserBookData = otherUserBookDataRes.success
+//             ? otherUserBookDataRes.data
+//             : [];
+
+//         if (!otherUserProfile || !otherUserBookData) {
+//             notFound();
+//         }
+
+//         return (
+//             <OtherUserProfilePage
+//                 otherUserBookData={otherUserBookData}
+//                 initialUserData={otherUserProfile.data}
+//             />
+//         );
+//     } catch (error) {
+//         notFound();
+//     }
+// }
+
 export default async function UserProfilePage({
     params,
 }: {
@@ -16,7 +45,8 @@ export default async function UserProfilePage({
             ? otherUserBookDataRes.data
             : [];
 
-        if (!otherUserProfile || !otherUserBookData) {
+        // Fix: Check the success property instead
+        if (!otherUserProfile.success || !otherUserProfile.data) {
             notFound();
         }
 
